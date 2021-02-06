e780b00782cd4b948349a2dbe0d82059acc7f77b
Jade3375 <36305507+floodlock@users.noreply.github.com>
2021-02-06 20:52:26 +0000

    added basic docs to test file

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,9 +1,12 @@
 import {IPC} from '../index'
 
+//client takes 2 arguments, your application ID (not discord), logger: true or false
 const client = new IPC("420907324945989632") 
 
+//start the IPC client
 client.login()
 
+//basic JSON body for the status
 let body = {
     cmd:"SET_ACTIVITY",
     args: {
@@ -21,9 +24,11 @@ let body = {
         buttons: [{label: 'discord', url: "https://discord.gg/uyEQkTT"}, {label: 'github', url: 'https://github.com/Jade3375/RichCord'}]
     },
     },
+    //nonce is required, can be any string
     nonce: "a76a42c0-802d-4c01-23c5-018d61f3499f"
 }
 
+//wait for client to be ready before  calling any functions
 client.on("ready", () => {
     client.setStatus(body)
 })
